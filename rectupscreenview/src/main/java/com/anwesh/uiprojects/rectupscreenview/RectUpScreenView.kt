35e508aa44b2696aefcf312d9c5eded920dbe218
Anwesh43 <anweshthecool0@gmail.com>
2019-01-29 11:18:19 +0530

    adding RusNode class

--- rectupscreenview/src/main/java/com/anwesh/uiprojects/rectupscreenview/RectUpScreenView.kt
+++ rectupscreenview/src/main/java/com/anwesh/uiprojects/rectupscreenview/RectUpScreenView.kt
@@ -114,4 +114,47 @@ class RectUpScreenView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RUSNode(var i : Int, val state : State = State()) {
+
+        private var next : RUSNode? = null
+        private var prev : RUSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RUSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRUSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update{
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RUSNode {
+            var curr : RUSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
